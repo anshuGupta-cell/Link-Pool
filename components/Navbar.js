691d8b17d4cb842325038a183921a2e69ed9ce25
Anshu Gupta Laptop <anshuguptacell@gmail.com>
2025-08-23 08:04:42 +0530

    project initialized

---
+++ components/Navbar.js
@@ -0,0 +1,24 @@
+import Link from "next/link"
+
+const Navbar = () => {
+
+    return (
+        <div className="nav-bar flex">
+            <div className="nav-item">
+                <Link href="/">
+                    BitLinks
+                </Link>
+            </div>
+            <div className="nav-item">
+                <Link href="/">Home</Link>
+                <Link href="/about">About</Link>
+                <Link href="/shorten">Shorten</Link>
+                <Link href="/contact-us">Contact Us</Link>
+                <Link href="/try-now">Try Now</Link>
+                <Link href="/Github">Github</Link>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
