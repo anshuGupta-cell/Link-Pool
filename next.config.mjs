998412686db8487024a79f03e6332f036f546718
Anshu Gupta Laptop <anshuguptacell@gmail.com>
2025-10-22 22:14:51 +0530

    suspense useSearch para

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+    experimental: {
+        missingSuspenseWithCsRBailout: false
+    }
+};
 
 export default nextConfig;
